c02b273a2fa63ebfd18dd6b4077d55aec889c784
Rahulpandya11 <137744389+Rahulpandya11@users.noreply.github.com>
2025-05-12 00:04:41 +0530

    Update About.js
    youtube link changed to embaded

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -86,7 +86,7 @@ const About = () => {
                             <iframe
                                 width="100%"
                                 height="500"
-                                src="https://youtu.be/3WpgFgRWgas?si=DBHvc7G7pueIMuPm"
+                                src="https://www.youtube.com/embed/3WpgFgRWgas?si=s5jHY6QPEaHyNARW&rel=0&autoplay=1"
                                 title="YouTube video player"
                                 frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
